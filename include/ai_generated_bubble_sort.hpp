573459d462cde841c49772867615a2a1cc6baf3c
Vlad Tupikin <vladtupikin7@gmail.com>
2026-05-09 23:52:36 +0300

    Test gemma4: Add AI generated bubble sort algorhythm

---
+++ include/ai_generated_bubble_sort.hpp
@@ -0,0 +1,33 @@
+/*
+Test application for different sort algorhythms.
+Copyright (C) 2026  Vladyslav Tupikin
+Co-authored by AI Assistant
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef AI_GENERATED_BUBBLE_SORT
+#define AI_GENERATED_BUBBLE_SORT
+
+#include "dk_sort_iface.hpp"
+
+namespace dk_sort
+{
+    class AiGeneratedBubbleSort : public DkSortIface {
+    public:
+        void DkSortArray(int* array, int size) override;
+    };
+}
+
+#endif
---
+++ src/ai_generated_bubble_sort.cpp
@@ -0,0 +1,43 @@
+/*
+Test application for different sort algorhythms.
+Copyright (C) 2026  Vladyslav Tupikin
+Co-authored by AI Assistant
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <algorithm>
+#include "ai_generated_bubble_sort.hpp"
+
+namespace dk_sort
+{
+    void AiGeneratedBubbleSort::DkSortArray(int* array, int size) {
+        if (!array || size <= 1) {
+            return;
+        }
+
+        for (int i = 0; i < size - 1; ++i) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; ++j) {
+                if (array[j] > array[j + 1]) {
+                    std::swap(array[j], array[j + 1]);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
